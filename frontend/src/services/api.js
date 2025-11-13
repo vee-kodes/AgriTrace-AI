3d90c8413ad690ea3e8d40d2fc77f14cbbfc5cf1
vee-kodes <violachepchirchir23@gmail.com>
2025-11-13 18:17:28 +0300

    chore: update API service to use dynamic backend URL from environment variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: '/', // Proxy will handle 'http://localhost:5000'
+  baseURL: import.meta.env.VITE_API_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
